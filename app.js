3a0a2d6de0e1636d1164f27879ddb6a18a53a228
neev <naveedbarna@gmail.com>
2024-12-16 16:08:33 +0100

    Ejercicio de manejo de archivos en Node.js

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-//Importamos el módulo
-const saludar = require(`./saludos`);
+const fs = require('fs');
 
-//Usamos la función
-console.log(saludar("Coco"));
+//Crear un archivo
+fs.writeFile('archivo.txt', 'Contenido inicial del arhivo.', (err) => {
+    if (err) throw err;
+    console.log('Archivo creado con éxito');
+
+    //Leer el archivo creado
+    fs.readFile('archivo.txt', 'utf8', (err, data)=>{
+        if (err) throw err;
+        console.log('Contenido leído:', data);
+
+        //Añadir contenido extra
+        fs.appendFile('archivo.txt', '\nContenido adicional.', (err) =>{
+            if (err) throw err;
+            console.log('Contenido añadido con éxito.');
+        });
+    });
+});

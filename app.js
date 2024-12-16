f1e40090fb732aa2e90180e5c2236289d6255bde
neev <naveedbarna@gmail.com>
2024-12-16 16:16:20 +0100

    Integración de módulos y manejo de archivos

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const fs = require('fs');
+const saludar = require('./saludos');
+
+//Usar la función saludar
+const mensaje = saludar("Coco");
 
 //Crear un archivo
-fs.writeFile('archivo.txt', 'Contenido inicial del arhivo.', (err) => {
+fs.writeFile('saludo.txt', mensaje, (err) => {
     if (err) throw err;
-    console.log('Archivo creado con éxito');
+    console.log('Archivo saludo.txt creado con éxito');
 
     //Leer el archivo creado
-    fs.readFile('archivo.txt', 'utf8', (err, data)=>{
+    fs.readFile('saludo.txt', 'utf8', (err, data)=>{
         if (err) throw err;
         console.log('Contenido leído:', data);
 
-        //Añadir contenido extra
-        fs.appendFile('archivo.txt', '\nContenido adicional.', (err) =>{
+        //Añadir más contenido
+        fs.appendFile('saludo.txt', '\n¡Nos vemos pronto!', (err) =>{
             if (err) throw err;
             console.log('Contenido añadido con éxito.');
         });
